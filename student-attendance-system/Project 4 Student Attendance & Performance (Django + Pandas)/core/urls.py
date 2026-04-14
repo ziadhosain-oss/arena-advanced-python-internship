95f99d7ee919aebea1c502e5a70281b02c1af3c3
Ziad Hosain <ziad.hosain@gmail.com>
2026-04-14 16:14:04 +0600

    Initial commit

--- student-attendance-system/Project 4 Student Attendance & Performance (Django + Pandas)/core/urls.py
+++ student-attendance-system/Project 4 Student Attendance & Performance (Django + Pandas)/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from students.views import mark_attendance, student_dashboard, export_performance_excel, export_performance_csv
 
 urlpatterns = [
+    path('', student_dashboard, name='home'),
     path('admin/', admin.site.urls),
     path('attendance/', mark_attendance, name='mark_attendance'),
     path('dashboard/', student_dashboard, name='dashboard'),
